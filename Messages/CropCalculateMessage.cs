40bb86584c975d33decfed958edde31ce93fd753
Danny <lingbo_wx@hotmail.com>
2017-05-02 20:12:03 +0800

    Signed-off-by: Danny <lingbo_wx@hotmail.com>

---
+++ Messages/CropCalculateMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeChat.Adapter.Messages
+{
+    public class CropCalculateMessage: BaseMsg
+    {
+        public string first { get; set; }
+        public string remark { get; set; }
+        public string keyword1 { get; set; }
+        public string keyword2 { get; set; }
+        public string keyword3 { get; set; }
+        public string keyword4 { get; set; }
+        public string keyword5 { get; set; }
+        public CropCalculateMessage(WeChatPayConfig _config, AccessToken token) : base(_config, token)
+        {
+            template_id = "vKkNNORiSJ7jg1WusrJiZZ3iAKyvqc93C6WZ49kS9hQ";
+        }
+    }
+}
---
+++ Messages/RefundNotificationMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeChat.Adapter.Messages
+{
+    public class RefundNotificationMessage:BaseMsg
+    {
+        public string first { get; set; }
+        public string remark { get; set; }
+        public string reason { get; set; }
+        public string refund { get; set; }
+        public RefundNotificationMessage(WeChatPayConfig _config, AccessToken token) : base(_config, token)
+        {
+            template_id = "4xUtbva04CAlG-IjLg4ag_VIm51I7NGGjFJj7Qs_9Ak";
+        }
+    }
+}
