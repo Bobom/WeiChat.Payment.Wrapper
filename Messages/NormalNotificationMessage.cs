6ba0c47b06a578eeb8e25bc7ddbb246635af51b5
Danny <lingbo_wx@hotmail.com>
2018-09-18 16:01:27 +0800

    Signed-off-by: Danny <lingbo_wx@hotmail.com>

---
+++ Messages/NormalNotificationMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeChat.Adapter.Messages
+{
+    public class NormalNotificationMessage : BaseMsg
+    {
+        public NormalNotificationMessage(WeChatPayConfig _config, AccessToken token) : base(_config, token)
+        {
+            template_id = "RsP_3nVu7U1HLF1I01rTdIjFk8HAFJbuJSt4Jsbcs3k";
+        }
+    }
+}
